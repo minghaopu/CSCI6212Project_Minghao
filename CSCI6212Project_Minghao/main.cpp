440526fb9b34c019e24482c2a098ca0766e301b8
roneilPMH <roneil_PMH@Hotmail.com>
2015-11-24 16:24:09 -0500

    Initial Commit

---
+++ CSCI6212Project_Minghao/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCI6212Project_Minghao
+//
+//  Created by Roneil on 15/11/24.
+//  Copyright © 2015年 Geroge Washington University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
